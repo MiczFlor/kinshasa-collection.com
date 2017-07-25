08bd20db55b019c5095b4afd5091ae1ed76cd6e6
Micz Flor <micz.flor@web.de>
2017-07-25 09:45:31 +0200

    adding command line to change robots noindex

---
+++ cli_noindex2index.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sed -i 's/name=\"robots\" content=\"noindex\"/name=\"robots\" content=\"index, follow\"/g' htdocs/*.html
+sed -i 's/name="robots" content="noindex"/name="robots" content="index, follow"/g' htdocs/texts/*.html
